13d2c6b7f3c9e81b704235048f42c9cc188a5790
Tatiana <ganzhinovat@yandex.ru>
2023-04-11 20:26:34 +0300

    refactor: use DRY principle

--- script/createTree.js
+++ script/createTree.js
@@ -1,18 +1,18 @@
 const root = document.querySelector('.root');
 
 const createTree = (arr) => {
+  const createElem = (elem) => {
+    const div = document.createElement('div');
+    div.textContent = elem.name;
+    div.className = (elem.icon === 'opened-folder') ? `root__level root__level_opened root__level-${elem.level}` : `root__level root__level-${elem.level}`;
+    root.append(div);
+  };
+
   arr.forEach(elem => {
     if (!elem.children) {
-      const div = document.createElement('div');
-      div.textContent = elem.name;
-      div.className = (elem.icon === 'opened-folder') ? `root__level root__level_opened root__level-${elem.level}` : `root__level root__level-${elem.level}`;
-      root.append(div);
+      createElem(elem);
     } else {
-      const div = document.createElement('div');
-      div.textContent = elem.name;
-      div.className = (elem.icon === 'opened-folder') ? `root__level root__level_opened root__level-${elem.level}` : `root__level root__level-${elem.level}`;
-      root.append(div);
-      
+      createElem(elem);
       createTree(elem.children);
     }
   })
