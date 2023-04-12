122cf0a7c368dfa67db85913bd18f1cb2ceac495
Tatiana <ganzhinovat@yandex.ru>
2023-04-12 12:03:07 +0300

    refactor: optimizing code

--- script/createTree.js
+++ script/createTree.js
@@ -1,18 +1,17 @@
 const root = document.querySelector('.root');
 
-const createTree = (arr) => {
-  const createElem = (elem) => {
-    const div = document.createElement('div');
-    div.textContent = elem.name;
-    div.className = (elem.icon === 'opened-folder') ? `root__level root__level_opened root__level-${elem.level}` : `root__level root__level-${elem.level}`;
-    root.append(div);
-  };
+const createElem = (elem) => {
+  const div = document.createElement('div');
+  div.textContent = elem.name;
+  div.className = (elem.icon === 'opened-folder') ? `root__level root__level_opened root__level-${elem.level}` : `root__level root__level-${elem.level}`;
+  root.append(div);
+};
 
+const createTree = (arr) => {
   arr.forEach(elem => {
-    if (!elem.children) {
-      createElem(elem);
-    } else {
-      createElem(elem);
+    createElem(elem);
+
+    if (elem.children) {
       createTree(elem.children);
     }
   })
